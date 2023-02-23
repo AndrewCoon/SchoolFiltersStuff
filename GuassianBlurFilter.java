a16a56e3a1410644c700f1af2c34fc7e00c9e600
Androo <you@example.com>
2023-02-23 09:52:14 -0700

    very borked

--- GuassianBlurFilter.java
+++ GuassianBlurFilter.java
@@ -1,15 +1,22 @@
 public class GuassianBlurFilter implements Filter {
     public void filter(PixelImage pi) {
         Pixel[][] data = pi.getData();
-        
-        for (int i = 0; i < data.length; i+=3) {
-            for (int j = 0; j < data[0].length; j+=3) {
-                data[i][j].setBlue(255-data[i][j].getBlue());
-                data[i][j].setGreen(255-data[i][j].getGreen());
-                data[i][j].setRed(255-data[i][j].getRed());
+        try {
+            for (int i = 0; i < data.length; i += 3) {
+                for (int j = 0; j < data[0].length - 3; j += 3) {
+                    int avgR = 0, avgB = 0, avgG = 0;
+
+                    for(int h=-1; h< 1; h++) {
+                        for(int c=-1; c< 1; c++) {
+                            avgR +=
+                        }
+                    }
+                }
             }
-        }   
-        
+        } catch (Exception e) {
+
+        }
+
         pi.setData(data);
     }
 }
