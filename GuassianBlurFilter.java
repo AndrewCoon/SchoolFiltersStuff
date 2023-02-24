f27e1171cf7e8d27a25e6c29020022096f00e865
Androo <84476053+Anduwu1@users.noreply.github.com>
2023-02-24 09:06:34 -0700

    Update GuassianBlurFilter.java

--- GuassianBlurFilter.java
+++ GuassianBlurFilter.java
@@ -1,12 +1,24 @@
 public class GuassianBlurFilter implements Filter {
     public void filter(PixelImage pi) {
         Pixel[][] data = pi.getData();
+        Pixel[][] ret = data;
         
-        for (int i = 0; i < data.length; i+=3) {
-            for (int j = 0; j < data[0].length; j+=3) {
-                data[i][j].setBlue(255-data[i][j].getBlue());
-                data[i][j].setGreen(255-data[i][j].getGreen());
-                data[i][j].setRed(255-data[i][j].getRed());
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                int avgR = 0;
+                int avgG = 0;
+                int avgB = 0; 
+                if(i==0&&j==0) {
+                    avgR += data[i][j].getRed() * 4;
+                    avgR += data[i+1][j].getRed() * 2;
+                    avgR += data[i][j+1].getRed() * 2;
+                    avgR += data[i+1][j+1].getRed();
+                }
+                
+                
+                avgR /= 16;
+                
+                ret[i][j].setRed(avgR);
             }
         }   
         
